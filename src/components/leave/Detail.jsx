be021cea0139d155588a66817dcb776568b6e6ec
josephmario247 <bartholomewjoseph81@gmail.com>
2024-10-14 22:53:48 +0100

    error fix

--- src/components/leave/Detail.jsx
+++ src/components/leave/Detail.jsx
@@ -28,7 +28,7 @@ const Detail = () => {
     }, [])
 const changeStatus = async (id, status) => {
     try {
-        const response = await axios.put(`http://localhost:5000/api/leave/${id}`,{status}, {
+        const response = await axios.put(`https://employee-ms-backend.vercel.app/api/leave/${id}`,{status}, {
             headers: {
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
@@ -47,7 +47,7 @@ const changeStatus = async (id, status) => {
             <div className='max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md'>
                 <h2 className='text-2xl font-bold mb-8 text-center'>Leave Details</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                    <div><img className='rounded-full border w-72' src={`http://localhost:5000/${leave.employeeId.userId.profileImage}`} alt="" /></div>
+                    <div><img className='rounded-full border w-72' src={`https://employee-ms-backend.vercel.app/${leave.employeeId.userId.profileImage}`} alt="" /></div>
                     <div>
                         <div className='flex space-x-3 mb-3'>
                             <p className="text-lg font-bold">Name:</p>
