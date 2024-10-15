d922e7ff28399b2d61f305ec6f96c59765176d4d
josephmario247 <bartholomewjoseph81@gmail.com>
2024-10-15 06:44:45 +0100

    profil img fix

--- src/components/employee/View.jsx
+++ src/components/employee/View.jsx
@@ -31,7 +31,7 @@ const View = () => {
             <div className='max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md'>
                 <h2 className='text-2xl font-bold mb-8 text-center'>Employee Details</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                    <div><img className='rounded-full border w-72' src={`http://localhost:5000/${employee.userId.profileImage}`} alt="" /></div>
+                    <div><img className='rounded-full border w-72' src={`https://employee-ms-backend.vercel.app/${employee.userId.profileImage}`} alt="" /></div>
                     <div>
                         <div className='flex space-x-3 mb-5'>
                             <p className="text-lg font-bold">Name:</p>
--- src/components/leave/Detail.jsx
+++ src/components/leave/Detail.jsx
@@ -47,7 +47,7 @@ const changeStatus = async (id, status) => {
             <div className='max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md'>
                 <h2 className='text-2xl font-bold mb-8 text-center'>Leave Details</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                    <div><img className='rounded-full border w-72' src={`https://employee-ms-backend.vercel.app/${leave.employeeId.userId.profileImage}`} alt="" /></div>
+                    <div><img className='rounded-full border w-72' src={`https://employee-ms-backend.vercel.app/${leave.employeeId.userId.profileImage}`} alt="Profile image" /></div>
                     <div>
                         <div className='flex space-x-3 mb-3'>
                             <p className="text-lg font-bold">Name:</p>
